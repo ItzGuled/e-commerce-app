f8d72fbc0aa69f76d8894469dd745564658be4f2
itzguled <itzguled55337@gmail.com>
2022-03-23 18:48:39 -0500

    cat model

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS e-commerce-app;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE e-commerce-app;
 
